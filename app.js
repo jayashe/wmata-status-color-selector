3c53375ab4f1a26d39eaf95173b4922f08d99c25
Jay Ashe <jay.ashe@cava.com>
2018-01-23 00:01:20 -0500

    Use const, map lines to colors

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-var request = require('request');
+const request = require('request');
 
-
-var options = {
+const options = {
   url: 'https://api.wmata.com/Incidents.svc/json/Incidents',
   headers: {
     'api_key': `${process.env.WMATA_API_KEY}`
@@ -9,25 +8,38 @@ var options = {
   method: 'GET'
 };
 
+const colorMap = {
+  "RD": "#FF0000",
+  "BL": "#0000FF",
+  "GR": "#00FF00",
+  "OR": "#FFA500",
+  "YL": "#FFFFE0",
+  "SV": "#FFFFFF"
+}
+
 function callback(error, response, body) {
   if (!error && response.statusCode == 200) {
-    var info = JSON.parse(body);
-    let set = new Set();
+    const info = JSON.parse(body);
+    const set = new Set();
     info.Incidents.forEach(function(incident) {
       lines = incident.LinesAffected.split(";");
       lines.forEach(function(line) {
         line = line.trim();
-        if (line != '') {
-          set.add(line);
+        if (line != '' && colorMap[line]) {
+          set.add(colorMap[line]);
         }
       });
     });
-    set.forEach(function(line) {
-      console.log('+++' + line + '+++');
+    set.forEach(function(color) {
+      sendAlert(color);
     });
   } else {
     console.log(error);
   }
+}
+
+function sendAlert(color) {
+  console.log(color);
 }
 
 request(options, callback);
